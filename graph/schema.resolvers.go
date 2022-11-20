046d2f1e787522b3bfee8bd4c5e4fb0adcef4710
ottolauncher <nnguionza@gmail.com>
2022-11-20 10:59:49 -0800

    fix input field on schema

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -11,14 +11,64 @@ import (
 	"github.com/ottolauncher/recipes/graph/model"
 )
 
-// CreateTodo is the resolver for the createTodo field.
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+// CreateIngredient is the resolver for the createIngredient field.
+func (r *mutationResolver) CreateIngredient(ctx context.Context, input model.NewIngredient) (*model.Ingredient, error) {
+	panic(fmt.Errorf("not implemented: CreateIngredient - createIngredient"))
 }
 
-// Todos is the resolver for the todos field.
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+// UpdateIngredient is the resolver for the updateIngredient field.
+func (r *mutationResolver) UpdateIngredient(ctx context.Context, input *model.UpdateIngredient) (*model.Ingredient, error) {
+	panic(fmt.Errorf("not implemented: UpdateIngredient - updateIngredient"))
+}
+
+// DeleteIngredient is the resolver for the deleteIngredient field.
+func (r *mutationResolver) DeleteIngredient(ctx context.Context, filter map[string]interface{}) (bool, error) {
+	panic(fmt.Errorf("not implemented: DeleteIngredient - deleteIngredient"))
+}
+
+// CreateRecipe is the resolver for the createRecipe field.
+func (r *mutationResolver) CreateRecipe(ctx context.Context, input model.NewRecipe) (*model.Recipe, error) {
+	panic(fmt.Errorf("not implemented: CreateRecipe - createRecipe"))
+}
+
+// UpdateRecipe is the resolver for the updateRecipe field.
+func (r *mutationResolver) UpdateRecipe(ctx context.Context, input model.UpdateRecipe) (*model.Recipe, error) {
+	panic(fmt.Errorf("not implemented: UpdateRecipe - updateRecipe"))
+}
+
+// DeleteRecipe is the resolver for the deleteRecipe field.
+func (r *mutationResolver) DeleteRecipe(ctx context.Context, filter map[string]interface{}) (bool, error) {
+	panic(fmt.Errorf("not implemented: DeleteRecipe - deleteRecipe"))
+}
+
+// Ingredient is the resolver for the ingredient field.
+func (r *queryResolver) Ingredient(ctx context.Context, filter map[string]interface{}) (*model.Ingredient, error) {
+	panic(fmt.Errorf("not implemented: Ingredient - ingredient"))
+}
+
+// Ingredients is the resolver for the ingredients field.
+func (r *queryResolver) Ingredients(ctx context.Context, filter map[string]interface{}, limit *int, page *int) ([]*model.Ingredient, error) {
+	panic(fmt.Errorf("not implemented: Ingredients - ingredients"))
+}
+
+// Recipe is the resolver for the recipe field.
+func (r *queryResolver) Recipe(ctx context.Context, filter map[string]interface{}) (*model.Recipe, error) {
+	panic(fmt.Errorf("not implemented: Recipe - recipe"))
+}
+
+// Recipes is the resolver for the recipes field.
+func (r *queryResolver) Recipes(ctx context.Context, filter map[string]interface{}, limit *int, page *int) ([]*model.Recipe, error) {
+	panic(fmt.Errorf("not implemented: Recipes - recipes"))
+}
+
+// Search is the resolver for the search field.
+func (r *queryResolver) Search(ctx context.Context, filter map[string]interface{}, limit *int, page *int) ([]model.SearchRecipeResult, error) {
+	panic(fmt.Errorf("not implemented: Search - search"))
+}
+
+// Recipe is the resolver for the recipe field.
+func (r *subscriptionResolver) Recipe(ctx context.Context) (<-chan *model.Recipe, error) {
+	panic(fmt.Errorf("not implemented: Recipe - recipe"))
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -27,5 +77,22 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// Subscription returns generated.SubscriptionResolver implementation.
+func (r *Resolver) Subscription() generated.SubscriptionResolver { return &subscriptionResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type subscriptionResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+}
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	panic(fmt.Errorf("not implemented: Todos - todos"))
+}
