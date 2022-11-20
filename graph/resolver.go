f1420cefd259a4c7abc5544cf45b66a5ffeba502
ottolauncher <nnguionza@gmail.com>
2022-11-20 11:15:43 -0800

    implement secureless server for now

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,13 @@
+//go:generate go run github.com/99designs/gqlgen generate
 package graph
+
+import db "github.com/ottolauncher/recipes/graph/db/mongo"
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	RM *db.RecipeManager
+	IM *db.IngredientManager
+}
--- server.go
+++ server.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/ottolauncher/recipes/graph"
+	db "github.com/ottolauncher/recipes/graph/db/mongo"
 	"github.com/ottolauncher/recipes/graph/generated"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 const defaultPort = "8080"
@@ -19,11 +28,62 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "header:X-XSRF-TOKEN",
+	}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	var (
+		once sync.Once
+		dao  *mongo.Client
+	)
+
+	once.Do(func() {
+		dao = db.Init()
+	})
+
+	src := dao.Database("recipes")
+	defer func() {
+		if err := dao.Disconnect(context.TODO()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	rm := db.NewRecipeManager(src)
+	im := db.NewIngredientManager(src)
+
+	config := generated.Config{Resolvers: &graph.Resolver{RM: rm, IM: im}}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(config))
+
+	e.GET("/playground", func(c echo.Context) error {
+		playground.Handler("GraphQL playground", "/query").ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+
+	e.POST("/query", func(c echo.Context) error {
+		srv.ServeHTTP(c.Response(), c.Request())
+		return nil
+	})
+	h2s := &http2.Server{
+		MaxConcurrentStreams: 250,
+		MaxReadFrameSize:     1048576,
+		IdleTimeout:          10 * time.Second,
+	}
+
+	s := http.Server{
+		Addr:    ":" + port,
+		Handler: h2c.NewHandler(e, h2s),
+	}
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
