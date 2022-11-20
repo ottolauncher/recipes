c56a8036599ac1f32d305c500a1eacebd75e813f
ottolauncher <nnguionza@gmail.com>
2022-11-20 10:34:41 -0800

    change main schema file

---
+++ preloads/preloads.go
@@ -0,0 +1,33 @@
+package preloads
+
+import (
+	"context"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
+func GetPreloads(ctx context.Context) []string {
+	return GetNestedPreloads(
+		graphql.GetOperationContext(ctx),
+		graphql.CollectFieldsCtx(ctx, nil),
+		"",
+	)
+}
+
+func GetNestedPreloads(ctx *graphql.OperationContext, fields []graphql.CollectedField, prefix string) (preloads []string) {
+	for _, column := range fields {
+		prefixColumn := GetPreloadString(prefix, column.Name)
+		preloads = append(preloads, prefixColumn)
+		preloads = append(preloads,
+			GetNestedPreloads(ctx, graphql.CollectFields(ctx, column.Selections, nil), prefixColumn)...)
+	}
+	return
+}
+
+// GetPreloadString collect whished returning field on query
+func GetPreloadString(prefix string, name string) string {
+	if len(prefix) > 0 {
+		return prefix + "." + name
+	}
+	return name
+}
